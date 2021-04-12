343a3f7335f8ff3b62819d995521e9aa919a9e7b
wtlin1228 <wtlin1228@gmail.com>
2021-04-12 23:43:42 +0800

    initial commit

---
+++ apps/graphql-server/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'graphql-server',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/graphql-server',
+};
