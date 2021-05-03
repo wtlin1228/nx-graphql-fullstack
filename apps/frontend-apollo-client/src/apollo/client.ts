bf0efeffa136357684e55e26e06b423aaeb53cc7
wtlin1228 <wtlin1228@gmail.com>
2021-05-03 14:01:32 +0800

    feat(frontend-apollo-client): add error link to track errors

--- apps/frontend-apollo-client/src/apollo/client.ts
+++ apps/frontend-apollo-client/src/apollo/client.ts
@@ -1,11 +1,13 @@
 import { ApolloClient, ApolloLink, InMemoryCache } from '@apollo/client';
 import createAuthLink from './links/auth';
 import createDelayLink from './links/delay';
+import createErrorLink from './links/error';
 import createHttpLink from './links/http';
 
 const initApolloClient = () =>
   new ApolloClient({
     link: ApolloLink.from([
+      createErrorLink(),
       createDelayLink(),
       createAuthLink(),
       createHttpLink(),
---
+++ apps/frontend-apollo-client/src/apollo/links/error.ts
@@ -0,0 +1,14 @@
+import { onError } from '@apollo/client/link/error';
+
+export default function createErrorLink() {
+  return onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.forEach(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+        )
+      );
+
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  });
+}
